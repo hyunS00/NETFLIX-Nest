ea389bc38399f0eb884b0ab15b218e3087628f19
hyunS00 <optifun72@gmail.com>
2025-01-07 19:17:00 +0900

    Movie API 설계

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Patch, Post, Delete } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,50 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getMovies() {
+    return [
+      {
+        id: 1,
+        name: '아케인',
+        character: ['바이', '징크스'],
+      },
+      {
+        id: 2,
+        name: '스토브리그',
+        character: ['남궁민'],
+      },
+    ];
+  }
+
+  @Get()
+  getMovie() {
+    return {
+      id: 1,
+      name: '아케인',
+      character: ['바이', '징크스'],
+    };
+  }
+
+  @Post()
+  postMovie() {
+    return {
+      id: 3,
+      name: '어벤저스',
+      character: ['아이언맨', '캡틴아메리카'],
+    };
+  }
+
+  @Patch()
+  patchMovie() {
+    return {
+      id: 3,
+      name: '어벤저스',
+      character: ['아이언맨', '블랙위도우'],
+    };
+  }
+
+  @Delete()
+  deleteMove() {
+    return 3;
   }
 }
