eb7f32aeca12081c47ade832c9b7153ce4a30a06
hyunS00 <optifun72@gmail.com>
2025-01-07 19:59:17 +0900

    PATCH movie id 구현

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -58,12 +58,15 @@ export class AppController {
   }
 
   @Patch(':id')
-  patchMovie() {
-    return {
-      id: 3,
-      name: '어벤저스',
-      character: ['아이언맨', '블랙위도우'],
-    };
+  patchMovie(@Param('id') id: string, @Body('title') title: string) {
+    const movie = this.movies.find((m) => m.id === +id);
+    if (!movie) {
+      throw new NotFoundException('존재하지 않는 영화 ID입니다.');
+    }
+
+    Object.assign(movie, { title });
+
+    return movie;
   }
 
   @Delete(':id')
