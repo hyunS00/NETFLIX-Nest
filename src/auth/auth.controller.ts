40294afb28bee4821ad53373d9d029d53024fd4b
hyunS00 <optifun72@gmail.com>
2025-01-16 17:00:00 +0900

    로컬 회원가입 구연

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Headers } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  /// authorization: Basic $token
+  registerUser(@Headers('authorization') token: string) {
+    return this.authService.register(token);
+  }
 }
