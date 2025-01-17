d0c926ebcc3ca484b34369b484290e208a24e529
hyunS00 <optifun72@gmail.com>
2025-01-17 17:23:17 +0900

    Jwt Strategy 적용

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,14 @@
-import { Controller, Post, Headers } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Headers,
+  Request,
+  UseGuards,
+  Get,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './strategy/local.strategy';
+import { JwtAuthGaurd } from './strategy/jwt.strategy';
 
 @Controller('auth')
 export class AuthController {
@@ -15,5 +24,20 @@ export class AuthController {
   /// authorization: Basic $token
   loginUser(@Headers('authorization') token: string) {
     return this.authService.login(token);
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login/passport')
+  async logUserPassport(@Request() req) {
+    return {
+      refreshToken: await this.authService.issueToken(req.user, true),
+      accessToken: await this.authService.issueToken(req.user, false),
+    };
+  }
+
+  @UseGuards(JwtAuthGaurd)
+  @Get('private')
+  async private(@Request() req) {
+    return req.user;
   }
 }
