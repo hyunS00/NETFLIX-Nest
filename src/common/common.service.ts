6ff3f98928531b332c6bda4465c5fad320f71868
hyunS00 <optifun72@gmail.com>
2025-01-22 13:26:05 +0900

    멀티 컬럼 Cursor Based Pagination

--- src/common/common.service.ts
+++ src/common/common.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { SelectQueryBuilder } from 'typeorm';
 import { PagePaginationDto } from './dto/page-pagination.dto';
 import { CursorPaginationDto } from './dto/cursor-pagination.dto';
@@ -18,22 +18,104 @@ export class CommonService {
     qb.take(take).skip(skip);
   }
 
-  applyCursorPaginationParamsToQb<T>(
+  async applyCursorPaginationParamsToQb<T>(
     qb: SelectQueryBuilder<T>,
     dto: CursorPaginationDto,
   ) {
-    const { order, take, id } = dto;
+    const { cursor, take } = dto;
+    let { order } = dto;
 
-    if (id) {
-      const direction = order === 'ASC' ? '>' : '<';
+    if (cursor) {
+      const decodedCursor = Buffer.from(cursor, 'base64').toString('utf-8');
 
-      /// order -> ASC : movie.id > :id
-      /// :id
-      qb.where(`${qb.alias}.id ${direction} :id`, { id });
+      /**
+       * {
+       *  values: {
+       *    id: 27
+       *  },
+       * order: ['id_DESC']
+       * }
+       */
+      const cursorObj = JSON.parse(decodedCursor);
+
+      order = cursorObj.order;
+
+      const { values } = cursorObj;
+
+      /// (colunm1, column2, column3) > (value1, value2, value3)
+      const columns = Object.keys(values);
+      const comparisonOperator = order.some((o) => o.endsWith('DESC'))
+        ? '<'
+        : '>';
+      const whereConditions = columns.map((c) => `${qb.alias}.${c}`).join(',');
+      const whereParams = columns.map((c) => `:${c}`).join(',');
+
+      qb.where(
+        `(${whereConditions}) ${comparisonOperator} (${whereParams})`,
+        values,
+      );
     }
 
-    qb.orderBy(`${qb.alias}.id`, order);
+    // [ "likeCount_DESC", "id_DESC"]
+    for (let i = 0; i < order.length; i++) {
+      const [column, direction] = order[i].split('_');
+
+      if (direction !== 'ASC' && direction !== 'DESC') {
+        throw new BadRequestException('Order는 ASC 또는 DESC으로 입력해주세요');
+      }
+
+      if (i === 0) {
+        qb.orderBy(`${qb.alias}.${column}`, direction);
+      } else {
+        qb.addOrderBy(`${qb.alias}.${column}`, direction);
+      }
+    }
+    // if (id) {
+    //   const direction = order === 'ASC' ? '>' : '<';
+
+    //   /// order -> ASC : movie.id > :id
+    //   /// :id
+    //   qb.where(`${qb.alias}.id ${direction} :id`, { id });
+    // }
+
+    // qb.orderBy(`${qb.alias}.id`, order);
 
     qb.take(take);
+
+    const results = await qb.getMany();
+
+    const nextCursor = this.generateNextCursor(results, order);
+
+    return { qb, nextCursor };
+  }
+
+  generateNextCursor<T>(results: T[], order: string[]): string | null {
+    if (results.length === 0) return null;
+
+    /**
+     * {
+     *  values: {
+     *    id: 27
+     *  },
+     * order: ['id_DESC']
+     * }
+     */
+
+    const lastItem = results[results.length - 1];
+
+    const values = {};
+
+    order.forEach((columnOrder) => {
+      const [column] = columnOrder.split('_');
+      values[column] = lastItem[column];
+    });
+
+    const cursorObj = { values, order };
+
+    const nextCursor = Buffer.from(JSON.stringify(cursorObj)).toString(
+      'base64',
+    );
+
+    return nextCursor;
   }
 }
