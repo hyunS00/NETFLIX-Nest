cfbed56c083542579c74c65656b681b22b24354a
hyunS00 <optifun72@gmail.com>
2025-01-23 18:06:43 +0900

    file 업로드 로직 commonModule로 이동

--- src/movie/movie.module.ts
+++ src/movie/movie.module.ts
@@ -7,31 +7,27 @@ import { MovieDetail } from './entity/movie-detail.entity';
 import { Director } from 'src/director/entitie/director.entity';
 import { Genre } from 'src/genre/entitie/genre.entity';
 import { CommonModule } from 'src/common/common.module';
-import { MulterModule } from '@nestjs/platform-express';
-import { diskStorage } from 'multer';
-import { join } from 'path';
-import { v4 } from 'uuid';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Movie, MovieDetail, Director, Genre]),
     CommonModule,
-    MulterModule.register({
-      storage: diskStorage({
-        destination: join(process.cwd(), 'public', 'movie'),
-        filename: (req, file, cb) => {
-          const split = file.originalname.split('.');
+    // MulterModule.register({
+    //   storage: diskStorage({
+    //     destination: join(process.cwd(), 'public', 'movie'),
+    //     filename: (req, file, cb) => {
+    //       const split = file.originalname.split('.');
 
-          let extention = 'mp4';
+    //       let extention = 'mp4';
 
-          if (split.length > 1) {
-            extention = split[split.length - 1];
-          }
+    //       if (split.length > 1) {
+    //         extention = split[split.length - 1];
+    //       }
 
-          cb(null, `${v4()}_${Date.now()}.${extention}`);
-        },
-      }),
-    }),
+    //       cb(null, `${v4()}_${Date.now()}.${extention}`);
+    //     },
+    //   }),
+    // }),
   ],
   controllers: [MovieController],
   providers: [MovieService],
