0ec66cbe5562a4868f6e80e3fdaff9f692dc0e54
hyunS00 <optifun72@gmail.com>
2025-01-26 12:24:42 +0900

    ApiProperty 에노테이션 적용

--- src/movie/dto/create-movie.dto.ts
+++ src/movie/dto/create-movie.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import {
   ArrayNotEmpty,
@@ -10,14 +11,26 @@ import {
 export class CreateMovieDto {
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({
+    description: '영화 제목',
+    example: '겨울왕국',
+  })
   title: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({
+    description: '영화 설명',
+    example: '3시간 훅가요',
+  })
   detail: string;
 
   @IsNotEmpty()
   @IsNumber()
+  @ApiProperty({
+    description: '감독 객체ID',
+    example: 1,
+  })
   directorId: number;
 
   @IsArray()
@@ -29,8 +42,16 @@ export class CreateMovieDto {
     },
   )
   @Type(() => Number)
+  @ApiProperty({
+    description: '장르 IDs',
+    example: [1, 2, 3],
+  })
   genreIds: number[];
 
   @IsString()
+  @ApiProperty({
+    description: '영화 파일 이름',
+    example: 'aaa-bbb-ccc-ddd.jpg',
+  })
   movieFileName: string;
 }
