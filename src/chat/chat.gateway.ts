1b00453d18a031e9ec945201e94b9c2b3fd753ff
hyunS00 <optifun72@gmail.com>
2025-02-08 12:21:41 +0900

    websocket 인증

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,15 +1,42 @@
 import {
   ConnectedSocket,
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
   SubscribeMessage,
   WebSocketGateway,
 } from '@nestjs/websockets';
 import { ChatService } from './chat.service';
 import { Socket } from 'socket.io';
+import { AuthService } from 'src/auth/auth.service';
 
 @WebSocketGateway()
-export class ChatGateway {
-  constructor(private readonly chatService: ChatService) {}
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  constructor(
+    private readonly chatService: ChatService,
+    private readonly authService: AuthService,
+  ) {}
+
+  handleDisconnect(client: Socket) {
+    return;
+  }
+
+  async handleConnection(client: Socket) {
+    try {
+      const rawToken = client.handshake.headers.authorization;
+
+      const payload = await this.authService.parseBearerToken(rawToken, false);
+
+      if (payload) {
+        client.data.user = payload;
+      } else {
+        client.disconnect();
+      }
+    } catch (e) {
+      console.log(e);
+      client.disconnect();
+    }
+  }
 
   @SubscribeMessage('receiveMessage')
   async receiveMessage(
